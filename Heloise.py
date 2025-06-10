11140e462ddfb63a11a8793b185ada54290458de
Doritos555 <joaopedrotavaresdeassis57@gmail.com>
2025-06-10 19:22:03 -0300

    Add files via upload

---
+++ Heloise.py
@@ -0,0 +1,86 @@
+import streamlit as st
+from PIL import Image
+import random
+
+def menu():
+    
+    st.title("Para Heloise")
+    st.write("""
+             OIII BB <3
+              """)
+
+    if 'ms' not in st.session_state:
+        st.session_state.ms = []
+        st.session_state.ts = []
+        st.session_state.r = 0
+
+    st.sidebar.header("MENU")
+
+    st.image("https://i.pinimg.com/736x/87/31/08/8731088a968b11bc574e19413a472ce5.jpg")
+
+    opcao = st.sidebar.selectbox(
+        "SELECIONE A FUNÇÃO:",
+        options=[
+            "Mulher mais linda do mundo",
+            "Cantada misteriosa",
+            "Presente misterioso",
+            "Musica do Stray Kids",
+            "Pica Pau",
+            "Segredo"
+        ]
+    )
+
+    if opcao == "Mulher mais linda do mundo":
+        if st.sidebar.button("Executar"):
+            caminho_imagem = r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\Fofa.jpeg"
+
+            imagem = Image.open(r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\Fofa.jpeg")
+            st.image(imagem, caption="A mulher mais linda do mundo 💖")
+
+    elif opcao == "Cantada misteriosa":
+        if st.sidebar.button("Executar"):
+            cantadas = [
+                "Seu nome é Wi-Fi? Porque eu sinto uma conexão.",
+                "Você não é Google, mas tem tudo que eu procuro.",
+                "Você é feita de cobre e telúrio? Porque você é Cu-Te.",
+                "Se beleza fosse tempo, você seria a eternidade.",
+                "Se eu fosse um gato, passaria todas as minhas 7 vidas com você."
+            ]
+            frase_escolhida = random.choice(cantadas)
+            st.success(frase_escolhida)
+
+    elif opcao == "Presente misterioso":
+        if st.sidebar.button("Executar"):
+            imagens_presentes = [
+                r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\presente1.png",
+                r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\presente2.png",
+                r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\presente3.png",
+                r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\presente4.png",
+                r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\presente5.png"
+            ]
+            imagem_escolhida = random.choice(imagens_presentes)
+
+            try:
+                img = Image.open(imagem_escolhida)
+                st.image(img, caption="Seu presente misterioso 🎁", use_column_width=True)
+            except Exception as e:
+                st.error(f"Erro ao abrir imagem: {e}")
+
+    elif opcao == "Musica do Stray Kids":
+        if st.sidebar.button("Executar"):
+            st.video("https://youtu.be/NNi34HGhbao?si=bg41Loe9J5tX9eIw")
+
+    elif opcao == "Pica Pau":
+        if st.sidebar.button("Executar"):
+            st.video("https://youtu.be/Zqk9eTtZpzY?si=Oz-pg_zVh_IGF39a") 
+
+    elif opcao == "Segredo":
+        if st.sidebar.button("Executar"):
+            caminho_imagem = r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\te amo.jpg"
+            imagem = Image.open(r"C:\Users\marty\OneDrive\Documentos\MatTec\Heloise\te amo.jpg")
+            st.image(imagem, caption="Te amo 💖")
+
+
+
+if __name__ == '__main__':
+    menu()
